54a3841733a4faae498689bbe0c6827405ee1024
Juan Carlos Ferriolo <jcfd@adinet.com.uy>
2024-11-04 12:37:51 -0300

    Update index.js

--- sentiment/index.js
+++ sentiment/index.js
@@ -38,20 +38,27 @@ app.post('/sentiment', async (req, res) => {
     try {
         const analysisResult = analyzer.getSentiment(sentence.split(' '));
 
-        let sentiment = "neutral";
+       let sentiment = "neutral";
 
         // Task 5: set sentiment to negative or positive based on score rules
-        {{insert code here}}
+        // {{insert code here}}
+        if (analysisResult < 0) {
+            sentiment = "negative";
+        } else if (analysisResult > 0.33) {
+            sentiment = "positive";
+        }
 
         // Logging the result
         logger.info(`Sentiment analysis result: ${analysisResult}`);
 
-        // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
-        {{insert code here}}
+        // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment });
+        // {{insert code here}}
+        res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
-        // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
-        {{insert code here}}
+        // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'});
+        // {{insert code here}}
+        res.status(500).json({ message: 'Error performing sentiment analysis' });
     }
 });
 
