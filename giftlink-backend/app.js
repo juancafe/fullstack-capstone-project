3bef7b0789293eb0995ee18ad8ad7363a433303a
juancafe <jcfd@adinet.com.uy>
2024-11-08 10:40:25 -0500

    Develop Back-end Integration for Registratione

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -22,11 +22,9 @@ app.use(express.json());
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
-
+const authRoutes = require('./routes/authRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
 const searchRoutes = require('./routes/searchRoutes');
-
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
@@ -35,9 +33,8 @@ app.use(pinoHttp({ logger }));
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
-
+app.use('/api/auth', authRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
 app.use('/api/search', searchRoutes);
 
 // Global Error Handler
