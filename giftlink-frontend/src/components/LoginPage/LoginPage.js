c244be9575168b77f99ce262ac9a3ccdff161a9c
juancafe <jcfd@adinet.com.uy>
2024-11-12 08:51:34 -0500

    Implement Back-end in Login Component

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,15 +1,65 @@
 import React, { useState,useEffect } from 'react';
+import { urlConfig} from '../../config';  //Task 1: Import urlConfig from `giftlink-frontend/src/config.js`
+import { useAppContext } from '../../context/AuthContext';  //Task 2: Import useAppContext `giftlink-frontend/context/AuthContext.js`
+import { useNavigate } from 'react-router-dom'; //Task 3: Import useNavigate from `react-router-dom` to handle navigation after successful registration.
 
 import './LoginPage.css';
 
 function LoginPage() {
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [password, setPassword] = useState(''); 
+    const [incorrect, setIncorrect] = useState(''); //Task 4: Include a state for incorrect password.
+    //Task 5: Create a local variable for `navigate`,`bearerToken`   and `setIsLoggedIn`.
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+    //Task 6. If the bearerToken has a value (user already logged in), navigate to MainPage
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+        navigate('/app')
+        }
+    }, [navigate])
 
     const handleLogin = async (e) => {
         e.preventDefault();
-}
+        //api call
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            //Step 1 - Task 7
+            method: 'POST',
+            //Step 1 - Task 8
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+          },
+        //Step 1 - Task 9
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
+        });
 
+        //Step 2: Task 1
+        const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+            //Step 2: Task 2
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+            //Step 2: Task 3
+          setIsLoggedIn(true);
+            //Step 2: Task 4
+          navigate('/app');
+        } else {
+            //Step 2: Task 5
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
+      }
 
     return (
         <div className="container mt-5">
@@ -37,7 +87,10 @@ function LoginPage() {
                                 placeholder="Enter your password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
-                            />
+                            />                            
+                            
+                            //*Step 2: Task 6 Display an error message to the user.
+                            <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
                         </div>
                         {/* Include appropriate error message if login is incorrect*/}
                         <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
