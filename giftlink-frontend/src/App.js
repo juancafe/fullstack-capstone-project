168137bfaf08637fc6207108613ea145c6ea4ae8
Juan Carlos Ferriolo <jcfd@adinet.com.uy>
2024-11-14 08:17:36 -0300

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -12,7 +12,7 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 
 function App() {
-  const navigate = useNavigate();
+//  const navigate = useNavigate();
   return (
         <>
         <Navbar/>
